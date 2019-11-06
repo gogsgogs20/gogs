9b47a1eec79924d13d157e9588864e03cddf5a1a
root <gogsgogs20@gmail.com>
2019-11-06 04:31:42 -0500

    adding k8s files

---
+++ k8s/jenkins-entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# we have to do this in an entrypoint becuase the jenkins image has defined $JENKINS_HOME as a volume
+cp -R /usr/share/jenkins/jobs /var/jenkins_home/jobs
+
+# call the original entry point
+/sbin/tini -- /usr/local/bin/jenkins.sh
+
